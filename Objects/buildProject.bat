3c04e6061444c5f875c4ee29493a72dd0e3c088f
NurgalievAl <siriusiii1209@gmail.com>
2022-05-22 18:38:45 +0500

    Initial commit

---
+++ Objects/buildProject.bat
@@ -0,0 +1,3 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat"
+pushd "C:\flexGen\CodeGen\UKTIO_App\UKTIO_App\Objects"
+msbuild -restore "C:\flexGen\CodeGen\UKTIO_App\UKTIO_App\Objects\UKTIO_App.Objects.csproj"
